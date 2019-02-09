787e87ead50c6683032290c928fe79c01c4b3d1e
Anish Kumar <anish.kumar@upgrad.com>
2019-02-09 09:33:16 +0530

    adding express server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-app.use(express.static('dist'))
+app.use(express.static('.'))
 const port = process.env.PORT || 3001
 
 app.listen(port, ()=>{
